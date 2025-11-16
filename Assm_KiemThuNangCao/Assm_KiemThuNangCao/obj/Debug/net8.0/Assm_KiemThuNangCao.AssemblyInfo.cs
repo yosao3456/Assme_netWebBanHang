7592bc9b070c3e846c877abed8ef2a30fee43a37
yosao3456 <sngoc3480@gmail.com>
2025-11-16 14:47:02 +0700

    a

--- Assm_KiemThuNangCao/Assm_KiemThuNangCao/obj/Debug/net8.0/Assm_KiemThuNangCao.AssemblyInfo.cs
+++ Assm_KiemThuNangCao/Assm_KiemThuNangCao/obj/Debug/net8.0/Assm_KiemThuNangCao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assm_KiemThuNangCao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49542be80808e572dfb2a07bd4a9ba906d791ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5752dcb0d8f02e8891c2ab8d515f2ea679d14f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assm_KiemThuNangCao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assm_KiemThuNangCao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
