bc94c6e49d6a6be3636a21e91528fc1b0686dd78
Qa123-a <quocanhbo07@gmail.com>
2025-12-03 08:45:51 +0700

    b

---
+++ Assm_KiemThuNangCao/Assm_KiemThuNangCao/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Assm_KiemThuNangCao.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ Assm_KiemThuNangCao/Assm_KiemThuNangCao/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Assm_KiemThuNangCao.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+
+        public string Provider { get; set; } = "Local";
+        public string? ProviderId { get; set; }
+
+        public ICollection<Cart>? Carts { get; set; }
+    }
+}
