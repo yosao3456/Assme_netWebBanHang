26c17f7486e6ccb17793f9e117b40122d61df430
Qa123-a <quocanhbo07@gmail.com>
2025-12-03 08:42:45 +0700

    v

--- Assm_KiemThuNangCao/Assm_KiemThuNangCao/wwwroot/js/header.js
+++
@@ -1,6 +0,0 @@
-    function openMenu() {
-        document.getElementById("sideMenu").style.right = "0";
-    }
-    function closeMenu() {
-        document.getElementById("sideMenu").style.right = "-25%";
-    }
